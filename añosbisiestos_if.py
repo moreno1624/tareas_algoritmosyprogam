79b6882ab94b2c283269b970a64c2bffcda03454
miguel moreno <156612423+moreno1624@users.noreply.github.com>
2024-02-18 15:46:29 -0400

    Add files via upload

---
+++ añosbisiestos_if.py
@@ -0,0 +1,17 @@
+año= int(input("escriba un año entre 1900 y 2199: "))
+
+if año<1900 or año>2199:
+    print(" año no válido ")
+    quit()
+else:
+    pass
+if año%4==0 and año%100!=0 or  año%4==0 and año%100==0 and año%400==0:
+    
+    print("tu año es bisiesto")
+else:
+    print("tu año no es bisiesto")
+    
+añobisiesto= int((año-1900)//4-(año-1900)//100+(año-1900)//400)
+    
+print(añobisiesto)  
+
