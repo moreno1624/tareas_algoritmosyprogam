9815313466210f94c532b66844f6ee3580984baa
miguel moreno <156612423+moreno1624@users.noreply.github.com>
2024-02-20 19:13:50 -0400

    Add files via upload

--- clasesvehiculos.py
+++ clasesvehiculos.py
@@ -1,14 +1,24 @@
-CARROS="1"
-BARCOS="2"
-AVIONES="3"
-SALIR="4"
+x={}
+CARROS=1
+BARCOS=2
+AVIONES=3
+REGISTROS=4
+SALIR=5
 
+class seller:
+    def __init__( self, nombre):
+        self.nombre=nombre
+    
+    def __str__(self):
+        return f"concesionario:{self.nombre}"
+    
 class Vehiculos:
     def __init__(self, marca, modelo):
         self.marca= marca
         self.modelo= modelo
     def __str__(self):
         return f"marca: {self.marca},modelo: {self.modelo}"
+    
 class car(Vehiculos):
     def __init__(self, marca, modelo, color):
         Vehiculos.__init__(self, marca, modelo)
@@ -31,12 +41,13 @@ class avion(Vehiculos):
         return f"marca: {self.marca}, modelo: {self.modelo}, capacidad: {self.capacidad}"
     
 
+    
 def menu():
  print( f"""{CARROS})carros
 {BARCOS})barcos
 {AVIONES})aviones 
-{SALIR}) salir        """)
-
+{REGISTROS})registros
+{SALIR})salir        """)
 
      
 def main():
@@ -44,41 +55,72 @@ def main():
      print("="*35)
      menu()
      print("="*35)
-     Y= input("que vehículo quiere registrar: ")
-     if Y==CARROS :
-      Input(Y)
-      continue
-     elif Y==BARCOS:
-         Input(Y)
-         continue
-     elif Y==AVIONES:
-         Input(Y)
-         continue
+     Y= int(input("elija numero para continuar: "))
+     if Y <4:
+     
+      registro=input("como quiere nombrar el registro(no repetir): ")
+      print("="*35)
+      carac_1=input("cual es la marca de su vehiculo:  ")
+      carac_2= input("cual es el modelo de su vehiculo:  ")
+      
+      if Y==CARROS :
+          carros_input(registro,carac_1,carac_2)
+          
+          continue
+      elif Y==BARCOS:
+          barcos_input(registro,carac_1,carac_2)
+          
+          continue
+      elif Y==AVIONES:
+          avion_input(registro,carac_1,carac_2)
+          
+          continue
+     elif Y==REGISTROS:
+          registro_input()
      elif Y==SALIR :
          break
      else:
          print("ingrese un valor válido")
          continue
      
-def Input(Y):
- registro=input("como quiere nombrar el registro: ")
- carac_1=input(f"cual es la marca de su vehiculo:  ")
- carac_2= input(f"cual es el modelo de su vehiculo:  ")
- if Y=="1":
-     texto= " el color"
-     carac_3= input(f"cual es {texto} de su vehiculo:  ")
-     registro=car(carac_1,carac_2,carac_3)
-     return print(registro)
- elif Y=="2":
-     texto=" el tipo"
-     carac_3= input(f"cual es {texto} de su vehiculo:  ")
-     registro=barco(carac_1,carac_2,carac_3)
-     return print(registro)
- elif Y=="3":   
-     texto=" la capacidad"
-     carac_3= input(f"cual es {texto} de su vehiculo:  ")
-     registro=avion(carac_1,carac_2,carac_3)
-     return print(registro)
+
+
+def carros_input(registro,carac_1,carac_2):
+    
+ texto= " el color"
+ carac_3= input(f"cual es {texto} de su vehiculo:  ")
+ print("="*35)
+ carrito=car(carac_1,carac_2,carac_3)
+ x[registro]=[carrito.marca,carrito.modelo,carrito.color]
+ return print(carrito)
  
+def barcos_input(registro,carac_1,carac_2):
+    
+ texto=" el tipo"
+ carac_3= input(f"cual es {texto} de su vehiculo:  ")
+ print("="*35)
+ carrito=barco(carac_1,carac_2,carac_3)
+ x[registro]=[carrito.marca,carrito.modelo,carrito.tipo]
+ return print(carrito)
+
+def avion_input(registro,carac_1,carac_2):
+
+ texto=" la capacidad"
+ carac_3= input(f"cual es {texto} de su vehiculo:  ")
+ print("="*35)
+ carrito=avion(carac_1,carac_2,carac_3)
+ x[registro]=[carrito.marca,carrito.modelo,carrito.capacidad]
+ return print(carrito)
+
+def registro_input():
+    if len(x)>0:
+     registro=input("como se llama el registro buscado: ")
+     print("="*35)
+     if registro in x:
+         print(f"{registro}= {x[registro]}")
+     else:
+         print("no se encontró el registro")
+    else:
+        print("no ha hecho ningun registro")
  
 main()
